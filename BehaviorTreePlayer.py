45050aa5c93c8c853e45a0d6ab978b3b71c2d9e6
Lanteriso <214778990@qq.com>
2024-04-06 15:35:12 +0800

    8

--- BehaviorTreePlayer.py
+++ BehaviorTreePlayer.py
@@ -68,6 +68,8 @@ image_list4 = [
     ['resources/characterclass/hmz.png', 0.8, None, [[0, 0], '毁灭者']],
     ['resources/characterclass/emlr.png', 0.8, None, [[0, 0], '恶魔利刃']],
     ['resources/characterclass/kpds.png', 0.8, None, [[0, 0], '卡牌大师']],
+    ['resources/characterclass/cjz.png', 0.8, None, [[0, 0], '裁决者']],
+    ['resources/characterclass/zhs.png', 0.8, None, [[0, 0], '召唤师']],
 ]
 
 def RunBehaviorTree(player, monster):
